d73ef06d71789ec948a769636158ab70278514b0
Python Floyd <floyd.escuadra@ciit.edu.ph>
2024-09-18 10:38:12 +0800

    player details
    name, age, race, physicaldamage, armor, magicdamage, magicresistance, speed.

---
+++ Assets/Scripts/unityhub.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unityhub : MonoBehaviour
+{
+    public string Name = "shasha";
+    public int Age = 63;
+    public string Race = "Philam";
+    public int PhysicalDamage = 28;
+    public int Armor = 99;
+    public int MagicDamage = 70;
+    public int MagicResistance = 30;
+    public float Speed = 88f;
+
+}
