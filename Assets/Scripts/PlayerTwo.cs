2f9f9af9e3e733c70defd2943c2735f467fc26db
Python "Theo <izifeliz@gmail.com>
2024-09-18 10:45:42 +0800

    playertwo
    playertwo stats

---
+++ Assets/Scripts/PlayerTwo.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerTwo : MonoBehaviour
+{
+    public string Name = "Plum";
+    public int Age = 27;
+    public string Race = "Human";
+    public int PhysicalDmg = 25;
+    public int Armor = 5;
+    public int Magicdmg = 10;
+    public int Magicres = 5;
+    public float Speed = 102f;
+}
